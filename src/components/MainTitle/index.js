75d829f0133fcd5d90910d90964ddf738e24228a
msolesp <mariasolespain@MacBook-Pro-de-Maria-2.local>
2023-07-16 20:40:57 -0300

    first commit

---
+++ src/components/MainTitle/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './styles.css';
+
+class MainTitle extends Component {
+  static propTypes = {
+    title: PropTypes.string,
+  }
+  static defaultProps = {
+    title: 'Resultados de la encuesta de sueldos 2023.01',
+  }
+
+  render() {
+    return (
+      <div className='main-title-wrapper'>
+        <h1 className='main-title'>
+          <span className='first-line'>Resultados de la</span><br />
+          <span>Encuesta de Sueldos 2023.02</span><br />
+          <span alt="Segundo Semestre &trade;">Junio - Julio</span>
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default MainTitle;
